32dddcba12976de6bba67d4a932c1d64a9bb855e
thanhduongvs <thanh.duong.vs@gmail.com>
2020-08-26 09:30:09 +0700

    add 06

---
+++ swift 06 Destini/swift 06 Destini/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift 06 Destini
+//
+//  Created by Thanh Duong on 8/25/20.
+//  Copyright © 2020 Thanh Duong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
